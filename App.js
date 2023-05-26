cab7e7ec9a41ef9d77610a42f94285de31b2bc46
Keith Prakash <71426291+KeithPrakash@users.noreply.github.com>
2023-05-26 09:08:03 +0530

    1 bfapk

--- App.js
+++ App.js
@@ -1,20 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import * as SplashScreen from "expo-splash-screen";
+import { StatusBar, View, Text, StyleSheet, ActivityIndicator } from "react-native";
+ import { WebView } from "react-native-webview";
+import Box from '@mui/material/Box';
+import LinearProgress from '@mui/material/LinearProgress';
+import {CircularProgress} from "@mui/material";
+import * as Progress from "react-native-progress";
 
-export default function App() {
+const App = () => {
+  const [showProgress, setShowProgress] = useState(true);
+  const [showWebview, setShowWebview] = useState(false);
+
+  useEffect(() => {
+    // Simulating an API call or any other loading process
+    setTimeout(() => {
+      setShowProgress(false);
+      setShowWebview(true);
+      SplashScreen.hideAsync();
+    }, 1000);
+  }, []);
+  const RenderProgress = () => {
+    return (
+      <View style={styles.progressContainer}>
+        <ActivityIndicator
+          color="#009688"
+          size="large"
+          style={styles.activityIndicatorStyle}
+        />
+      </View>
+    );
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar hidden />
+      {showProgress && RenderProgress()}
+      {showWebview && (
+        <WebView
+          source={{ uri: "https://www.pettagama.lk/" }}
+          style={styles.webview}
+        />
+      )}
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  progressContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white",
+  },
+
+  webview: {
+    flex: 1,
+  },
+  activityIndicatorStyle: {
+    flex: 1,
+    position: "absolute",
+    marginLeft: "auto",
+    marginRight: "auto",
+    marginTop: "auto",
+    marginBottom: "auto",
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    justifyContent: "center",
   },
 });
+
+export default App;
